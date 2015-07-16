3fd9f389a350d1ab5d75602bdeeb59b896d11fbb
hansenaw <adamwesleyhansen@gmail.com>
2015-07-16 16:49:41 -0600

    jquery test

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 console.log("Running script...");
+
+$(document).ready(function() {
+	$('div').mouseenter(function() {
+		$(this).animate({
+			height: '+=10px';
+		});
+	});
+	$('div').mouseleave(function() {
+		$(this).animate({
+			height: '-=10px';
+		});
+	});
+	$('div').click(function() {
+		$(this).toggle(1000);
+	});
+});
