84dd1bb9053eefa26908be0aaca345de7454812b
hansenaw <adamwesleyhansen@gmail.com>
2015-07-17 16:46:03 -0600

    set ease time to 750

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -13,7 +13,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 500, 'easeInOutExpo');
+        }, 750, 'easeInOutExpo');
         event.preventDefault();
     });
 });
